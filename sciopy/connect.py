002a8486625f61e10c4f4ba110cb1abffb3be155
jt292 <jacob.thoenes@uni-rostock.de>
2023-01-04 16:50:37 +0100

    added serial read function, minor changes

--- sciopy/connect.py
+++ sciopy/connect.py
@@ -3,34 +3,51 @@ try:
 except ImportError:
     print("Could not import module: serial")
 
+import serial
+
 
 def available_serial_ports():
-    print("This function has to be programmed...")
+    """Lists serial port names
+
+    :raises EnvironmentError:
+        On unsupported or unknown platforms
+    :returns:
+        A list of the serial ports available on the system
+    """
+    if sys.platform.startswith("win"):
+        ports = ["COM%s" % (i + 1) for i in range(256)]
+    elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob("/dev/tty[A-Za-z]*")
+    elif sys.platform.startswith("darwin"):
+        ports = glob.glob("/dev/tty.*")
+    else:
+        raise EnvironmentError("Unsupported platform")
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
 
 
-def connect_COM_port(port: str = "COM5", baudrate: int = 9600, timeout: int = 1):
-    """Start a serial connection."""
+def connect_COM_port(port: str = "COM3", baudrate: int = 9600, timeout: int = 1):
+    """
+    Etablishes a serial connection to a given com port.
+    Returnes the serial connection
+    """
+
     ser = serial.Serial(
         port=port,
         baudrate=baudrate,
-        bytesize=serial.EIGHTBITS,
         parity=serial.PARITY_NONE,
-        timeout=timeout,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
     )
-    ser.flushInput()
 
-    try:
-        ser.isOpen()
-        print("Serial port is open")
-    except:
-        print("Error")
-        exit()
-
-    if ser.isOpen():
-        try:
-            while 1:
-                print(ser.read())
-        except Exception:
-            print("error")
-    else:
-        print("can not open port")
+    print("Connection to", ser.name, "is established.")
+    return ser
