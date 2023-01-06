c1e483bb056174782e65e676f310a929f16c93cb
jt292 <jacob.thoenes@uni-rostock.de>
2023-01-06 13:04:25 +0100

    modifications in dotpy

--- sciopy/doteit.py
+++ sciopy/doteit.py
@@ -2,6 +2,7 @@
 
 import os
 import numpy as np
+from dataclasses import dataclass, asdict
 
 # tmp path
 
@@ -10,35 +11,50 @@ path = "C:/Users/ScioSpecEIT/Desktop/data/20230105 14.36.10/setup/"
 src_list = [_ for _ in os.listdir(path) if _.endswith(".eit")]
 
 
+# @dataclass
 class SingleEitFrame:
-    pass
+    number_of_header: int
+    file_version_number: int
+    setup_name: str
+    date_time: str
+    f_min: float
+    f_max: float
+    f_scale: str  # f_scale = 'linear' if val == 0 else 'logarithmic'
+    f_count: int
+    current_amplitude: float
+    framerate: float
+    phase_correct_parameter: float
+    MeasurementChannels: list
+    MeasurementChannelsIndependentFromInjectionPattern: list
+    data: dict
+    # ...
 
 
 def single_eit_in_npz(name, l_path, s_path):
     with open(l_path + name, "r") as file:
         read_content = file.read().split("\n")
 
-    frame = SingleEitFrame()
-    # read_content[0]
-    # read_content[1]
-    frame.setup_name = read_content[2]
-    frame.date_time = read_content[3]
-    frame.f_min = read_content[4]
-    frame.f_max = read_content[5]
-    # read_content[6]
-    # read_content[7]
-    frame.current = read_content[8]
-    # read_content[9]
-    # read_content[10]
-    # read_content[11]
-    # read_content[12]
-    # read_content[13]
-    # read_content[14]
-    # read_content[15]
-    frame.MeasurementChannels = read_content[16].split(":")[1]
-    frame.MeasurementChannelsIndependentFromInjectionPattern = read_content[17].split(
-        ":"
-    )[1]
+    frame = SingleEitFrame(
+        #
+        #
+        read_content[2],
+        read_content[3],
+        read_content[4],
+        read_content[5],
+        #
+        #
+        read_content[8],
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        read_content[16].split(":")[1],
+        read_content[17].split(":")[1],
+        # el_data appended later
+    )
 
     for i in range(18, len(read_content) - 1, 2):
         el_cmb = read_content[i].split(" ")
@@ -46,11 +62,16 @@ def single_eit_in_npz(name, l_path, s_path):
         lct = read_content[i + 1].split("\t")
         lct = [ele.replace("E", "e") for ele in lct]
         lct = [float(ele) for ele in lct]
+        print("Set")
         setattr(frame, el_cmb, lct)
 
     sname = s_path + frame.setup_name + ".npz"
     print(sname)
     np.savez(str(sname), frame=frame)
+    sname = s_path + frame.setup_name + "dict.npz"
+    np.savez(str(sname), frame=asdict(frame))
 
 
 single_eit_in_npz(src_list[0], path, "C:/Users/ScioSpecEIT/Desktop/sciospeceit/sciopy/")
+
+# Convert the object to dictionary with function asdict()
