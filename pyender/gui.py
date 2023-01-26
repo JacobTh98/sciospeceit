ad50d07f5b1df350ebc50ca7ccd6296d53617fc6
JacobTh98 <jacob.thoenes@googlemail.com>
2023-01-26 09:57:31 +0100

    Implemented much more gui performance and movement

--- pyender/gui.py
+++ pyender/gui.py
@@ -8,7 +8,11 @@ from matplotlib.figure import Figure
 from matplotlib.patches import Rectangle
 from matplotlib.collections import PatchCollection
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+plt.rcParams["font.size"] = 5
+plt.rcParams["figure.autolayout"] = True
+
 
 # import serial.tools.list_ports
 from tkinter import (
@@ -47,7 +51,7 @@ btn_height = 50
 
 
 class Log:
-    def __init__(self, app):
+    def __init__(self, app) -> None:
         self.log = Text(app, height=10, width=100)
         self.log.place(x=10, y=590, width=500, height=200)
 
@@ -65,6 +69,16 @@ class Log:
 
 
 detected_com_ports = ["COM3", "COM4"]
+tank_architectures = ["select tank", "medium", "high"]
+step_width = [0.1, 1, 10]
+
+
+@dataclass
+class mmPerStep:
+    mm_per_step: Union[float, int]
+
+
+manual_step = mmPerStep(10)
 
 
 @dataclass
@@ -74,7 +88,27 @@ class Ender5Position:
     abs_x_pos: Union[int, float]
     abs_y_pos: Union[int, float]
     abs_z_pos: Union[int, float]
+    tank_architecture: Union[None, str]
     motion_speed: Union[int, float]
+    abs_x_tgt: Union[None, int, float]
+    abs_y_tgt: Union[None, int, float]
+    abs_z_tgt: Union[None, int, float]
+
+    # Probably add a limitbox for a corresponding tank
+    # TBD: Write a function that checks the boundary limit box
+
+
+# First Initialization
+enderstat = Ender5Position(
+    abs_x_pos=350,
+    abs_y_pos=350,
+    abs_z_pos=50,
+    tank_architecture=None,
+    motion_speed=1500,
+    abs_x_tgt=None,
+    abs_y_tgt=None,
+    abs_z_tgt=None,
+)
 
 
 @dataclass
@@ -130,6 +164,7 @@ class ConnectEnder5:
             print("Nothing")
 
     def connect_interact(self):
+        global enderstat
 
         self.connect_interact_button["text"] = "Connecting ..."
         print("Connection to ", str(self.com_dropdown.get()), "established.")
@@ -139,7 +174,71 @@ class ConnectEnder5:
         self.connect_interact_button["bg"] = "green"
         self.connect_interact_button["state"] = "disabled"
         self.com_dropdown["state"] = "disabled"
-        plot()
+
+        # Init Printer and drive to center:
+        # Also initialise current position
+        enderstat = Ender5Position(
+            abs_x_pos=0,
+            abs_y_pos=0,
+            abs_z_pos=100,
+            tank_architecture=None,
+            motion_speed=1500,
+            abs_x_tgt=180,
+            abs_y_tgt=180,
+            abs_z_tgt=100,
+        )
+        plot(enderstat)
+
+
+class TankSelect:
+    def __init__(self) -> None:
+        self.tnk_dropdown = ttk.Combobox(values=tank_architectures)
+        self.tnk_dropdown.current(0)
+        self.tnk_dropdown.bind("<<ComboboxSelected>>", self.dropdown_callback)
+        self.tnk_dropdown.place(
+            x=2 * spacer + btn_width + x_0ff,
+            y=spacer,
+            width=2 * btn_width,
+            height=btn_height,
+        )
+
+    def dropdown_callback(self, event=None):
+        if event:
+            tnk = self.tnk_dropdown.get()
+            print(tnk, "tank selected.")
+            enderstat.tank_architecture = tnk
+            plot(enderstat)
+        else:
+            pass
+
+
+class StepWidthSelect:
+    # global mm_per_step
+
+    def __init__(self, app) -> None:
+        self.stp_wdth_dropdown = ttk.Combobox(values=step_width)
+        self.stp_wdth_dropdown.current(2)
+        self.stp_wdth_dropdown.bind("<<ComboboxSelected>>", self.dropdown_callback)
+        self.stp_wdth_dropdown.place(
+            x=x_0ff + 4 * btn_width + spacer,
+            y=spacer,
+            width=btn_width,
+            height=btn_height,
+        )
+        self.mm_ps_label = Label(app, text="mm/step")
+        self.mm_ps_label.place(
+            x=x_0ff + 5 * btn_width + spacer,
+            y=spacer,
+            width=btn_width,
+            height=btn_height,
+        )
+
+    def dropdown_callback(self, event=None):
+        if event:
+            manual_step.mm_per_step = float(self.stp_wdth_dropdown.get())
+            print("Distance per step:", manual_step.mm_per_step)
+        else:
+            pass
 
 
 class MovementXYZ:
@@ -218,12 +317,12 @@ class MovementXYZ:
             height=btn_height,
         )
 
-        self.x_up_btn = Button(app, text="x+", command=self.move_x_up)
+        self.x_up_btn = Button(app, text="x+", command=self.move_x_down)
         self.x_up_btn.place(
             x=x_0ff, y=y_0ff + spacer + btn_height, width=btn_width, height=btn_height
         )
 
-        self.x_down_btn = Button(app, text="x-", command=self.move_x_down)
+        self.x_down_btn = Button(app, text="x-", command=self.move_x_up)
         self.x_down_btn.place(
             x=x_0ff + 2 * btn_width + 2 * spacer,
             y=y_0ff + spacer + btn_height,
@@ -239,7 +338,7 @@ class MovementXYZ:
             height=btn_height,
         )
 
-        self.z_center_btn = Button(app, text="center\nz")
+        self.z_center_btn = Button(app, text="center\nz", command=self.z_center)
         self.z_center_btn.place(
             x=x_0ff + 4 * btn_width + spacer,
             y=y_0ff + spacer + btn_height,
@@ -279,34 +378,50 @@ class MovementXYZ:
         )
 
     def set_x(self):
-        print(self.x_set_entry.get())
+        enderstat.abs_x_pos = float(self.x_set_entry.get())
+        plot(enderstat)
 
     def set_y(self):
-        print(self.y_set_entry.get())
+        enderstat.abs_y_pos = float(self.y_set_entry.get())
+        plot(enderstat)
 
     def set_z(self):
-        print(self.z_set_entry.get())
+        enderstat.abs_z_pos = float(self.z_set_entry.get())
+        plot(enderstat)
 
     def x_y_center(self):
-        pass
+        enderstat.abs_y_pos = 180
+        enderstat.abs_x_pos = 180
+        plot(enderstat)
 
     def move_x_up(self):
-        pass
+        enderstat.abs_x_pos += manual_step.mm_per_step
+        print(manual_step.mm_per_step)
+        plot(enderstat)
 
     def move_x_down(self):
-        pass
+        enderstat.abs_x_pos -= manual_step.mm_per_step
+        plot(enderstat)
 
     def move_y_up(self):
-        pass
+        enderstat.abs_y_pos += manual_step.mm_per_step
+        plot(enderstat)
 
     def move_y_down(self):
-        pass
+        enderstat.abs_y_pos -= manual_step.mm_per_step
+        plot(enderstat)
 
     def move_z_up(self):
-        pass
+        enderstat.abs_z_pos += manual_step.mm_per_step
+        plot(enderstat)
 
     def move_z_down(self):
-        pass
+        enderstat.abs_z_pos -= manual_step.mm_per_step
+        plot(enderstat)
+
+    def z_center(self):
+        enderstat.abs_z_pos = 100
+        plot(enderstat)
 
     def motion_speed_callback(self, event):
         print("Set motion speed to", self.motion_speed.get(), "[mm/min]")
@@ -376,10 +491,12 @@ class CreateCircularTrajectory:
             height=btn_height,
         )
 
-        self.compute_trajectory = Button(app, text="Info", command=compute_trajectory)
+        self.compute_trajectory = Button(
+            app, text="Set", command=compute_trajectory
+        )
         self.compute_trajectory.place(
             x=x_0ff + 4 * btn_width + spacer,
-            y=y_0ff + 4 * btn_height + 1 * spacer,
+            y=y_0ff + 5 * btn_height + 2 * spacer,
             width=btn_width,
             height=btn_height,
         )
@@ -397,47 +514,50 @@ def compute_trajectory():
     print("computation has to be substituted... (TBD)")
 
 
-"""
-def plot_x_y_circle(x,y):
-
-    plt.scatter(x,y, label="Measurement points", marker='*')
-    plt.legend()
-    plt.show()
-"""
-
-
-def plot(x=180, y=180, z=100) -> None:
-    plt.rcParams["font.size"] = 5
-    plt.rcParams["figure.autolayout"] = True
+def plot(enderstat: Ender5Position) -> None:
     fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(8, 5))
 
     ax1.set_title("Top view")
-    ax1.scatter(x, y, marker=".", label="Currently")
-    ax1.scatter(x + 10, y + 10, marker="*", label="Target")
+    ax1.scatter(enderstat.abs_x_pos, enderstat.abs_y_pos, marker=".")
+    if enderstat.abs_x_tgt is not None:
+        ax1.scatter(enderstat.abs_x_tgt, enderstat.abs_y_tgt, marker="*")
     ax1.set_ylabel("absolute y[mm]")
     ax1.set_xlabel("absolute x[mm]")
     ax1.set_xlim((0, 350))
     ax1.set_ylim((0, 350))
     ax1.grid()
-    ax1.legend(
-        loc="upper center",
-        bbox_to_anchor=(0.6, 1.00),
-        fancybox=True,
-        ncol=5,
-    )
-    if x == 180 and y == 180:
-        ax1.vlines(180, 0, 500, linestyles="dotted", color="black")
-        ax1.hlines(180, 0, 500, linestyles="dotted", color="black")
+    if enderstat.abs_x_pos == 180 and enderstat.abs_y_pos == 180:
+        ax1.vlines(180, 0, 500, linestyles="dotted", color="black", alpha=0.5)
+        ax1.hlines(180, 0, 500, linestyles="dotted", color="black", alpha=0.5)
 
     ax2.set_title("Front view")
-    errorboxes = [Rectangle((100, z), width=150, height=100)]
-    pc = PatchCollection(
-        errorboxes, facecolor="lightsteelblue", alpha=0.8, edgecolor="black"
+
+    if enderstat.tank_architecture is not None:
+        if enderstat.tank_architecture == "medium":
+            tank_archtctrs = [
+                Rectangle((100, enderstat.abs_z_pos), width=150, height=100)
+            ]
+            pc = PatchCollection(
+                tank_archtctrs, facecolor="lightsteelblue", alpha=0.8, edgecolor="black"
+            )
+            ax2.add_collection(pc)
+        if enderstat.tank_architecture == "high":
+            tank_archtctrs = [
+                Rectangle((100, enderstat.abs_z_pos), width=150, height=150)
+            ]
+            pc = PatchCollection(
+                tank_archtctrs, facecolor="lightsteelblue", alpha=0.8, edgecolor="black"
+            )
+            ax2.add_collection(pc)
+
+    ax2.hlines(
+        enderstat.abs_z_pos, 50, 300, linestyles="solid", color="black", label="z-table"
     )
-    ax2.add_collection(pc)
-    ax2.hlines(z, 50, 300, linestyles="solid", color="black", label="z-table")
-    ax2.scatter(x, z + 10, marker=".")
-    ax2.scatter(x + 10, z + 10, marker="*")
+    ax2.scatter(enderstat.abs_x_pos, 349, marker=".", label="Currently")
+    if enderstat.abs_z_tgt is not None:
+        ax2.scatter(
+            enderstat.abs_x_tgt, enderstat.abs_z_tgt, marker="*", label="Target"
+        )
     ax2.set_ylabel("absolute z[mm]")
     ax2.set_xlabel("absolute x[mm]")
     ax2.set_xlim((0, 350))
@@ -449,8 +569,7 @@ def plot(x=180, y=180, z=100) -> None:
     canvas = FigureCanvasTkAgg(fig, master=app)
     canvas.draw()
     canvas.get_tk_widget().place(x=750, y=spacer, width=400, height=800)
-    toolbar = NavigationToolbar2Tk(canvas, app)
-    toolbar.update()
+    plt.close(fig)
 
 
 def action_get_info_dialog():
@@ -482,6 +601,8 @@ app.grid()
 
 connect_ender_5 = ConnectEnder5(app)
 movement_xyz = MovementXYZ(app)
+tankselect = TankSelect()
+stepwidthselect = StepWidthSelect(app)
 create_circular_trajectory = CreateCircularTrajectory(app)
 LOG = Log(app)
 sys.stdout = LOG
@@ -497,7 +618,7 @@ dropdown.add_cascade(label="File", menu=datei_menu)
 dropdown.add_cascade(label="Help", menu=help_menu)
 
 
-plot()
+plot(enderstat)
 
 app.config(menu=dropdown)
 app.geometry("1200x900")
