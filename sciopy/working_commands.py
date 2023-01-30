145b0a968f1c6f46de0d720860db8e1f6d37fc06
jt292 <jacob.thoenes@uni-rostock.de>
2023-01-30 14:18:03 +0100

    Modified and substituated GeneralSystemMessage()

--- sciopy/working_commands.py
+++ sciopy/working_commands.py
@@ -1,56 +1,68 @@
 """ List of working commands. Just for an overview"""
 
-def GeneralSystemMessages(serial, msg_old:list = []): # Why returns this function none?!?!?!
-    """Reads the message buffer of a serial connection. Also prints out the general system message."""
+def SystemMessageCallback(serial, prnt_msg:bool = True, ret_hex_int:int=0):
+    """Reads the message buffer of a serial connection. Also prints out the general system message.
+    serial      ... serial connection
+    prnt_msg    ... print out the buffer
+    ret_hex_int ... Parameters -> ['none','hex', 'int', 'both']
+    
+    """
     msg_dict = {
-        b"\x02": "Timeout: Communication-timeout (less data than expected)",
-        b"\x04": "Wake-Up Message: System boot ready",
-        b"\x11": "TCP-Socket: Valid TCP client-socket connection",
-        b"\x81": "Not-Acknowledge: Command has not been executed",
-        b"\x82": "Not-Acknowledge: Command could not be recognized",
-        b"\x83": "Command-Acknowledge: Command has been executed successfully",
-        b"\x84": "System-Ready Message: System is operational and ready to receive data",
-        b"\x91": "Data holdup: Measurement data could not be sent via the master interface",
+        "0x01": "No message inside the message buffer",
+        "0x02": "Timeout: Communication-timeout (less data than expected)",
+        "0x04": "Wake-Up Message: System boot ready",
+        "0x11": "TCP-Socket: Valid TCP client-socket connection",
+        "0x81": "Not-Acknowledge: Command has not been executed",
+        "0x82": "Not-Acknowledge: Command could not be recognized",
+        "0x83": "Command-Acknowledge: Command has been executed successfully",
+        "0x84": "System-Ready Message: System is operational and ready to receive data",
+        "0x91": "Data holdup: Measurement data could not be sent via the master interface",
     }
-    msg_new = []
-   
-    start_cmd_tag = serial.read() # Read start tag.
-    cmd_msg_len = int.from_bytes(serial.read(), "big") # Read length of the message.
-    if msg_old: # msg_old has elements:
-        msg_new.append(start_cmd_tag)
-        msg_new = [serial.read() for i in range(cmd_msg_len)]
-        end_cmd_tag = serial.read()# Read message end tag.
-        msg_new.append(end_cmd_tag)
-        msg_old.append(msg_new) # Reads the serial message in Hex and adds to msg_old
-    else:
-        msg_new.append(start_cmd_tag)
-        msg_new = [serial.read() for i in range(cmd_msg_len)] # Reads the serial message in Hex.  
-        end_cmd_tag = serial.read()# Read message end tag.
-        msg_new.append(end_cmd_tag)
-        msg_old = msg_new
-    #msg_new = [int.from_bytes(m, "big") for m in msg_new] # Convert message to integer.
-    #if start_cmd_tag == end_cmd_tag: # Concludes with the comparison of start and end tag   
-    #print("start tag:",start_cmd_tag)
-    #print("msg_new len:",cmd_msg_len)
-    #print("end tag:",end_cmd_tag)
+    timeout_count = 0
+    received = []
+    received_hex = []
+    data_count = 0
+    
+    while True:
+        buffer = serial.read()
+        if buffer:
+            received.extend(buffer)
+            data_count += len(buffer)
+            timeout_count = 0
+            continue
+        timeout_count += 1
+        if timeout_count >= 1:
+            # Break if we haven't received any data
+            break
 
-    if end_cmd_tag != b'\x18':
-        print("Recall GeneralSystemMessages(serial, msg_new)\n")
-        GeneralSystemMessages(serial, msg_new)
-    if end_cmd_tag == b'\x18':
-        print(msg_dict[msg_new[0]])
-        print("msg_new:",msg_new)
-        print("msg_old:",msg_old)
-        return msg_old
+        received = ''.join(str(received))  # If you need all the data
+    received_hex = [hex(receive) for receive in received]
+    try:
+        msg_idx = received_hex.index('0x18')
+        print(msg_dict[received_hex[msg_idx+2]])
+    except BaseException:
+        print(msg_dict['0x01'])
+        prnt_msg = False
+    if prnt_msg:
+        print("message buffer:\n",received_hex)
+        print("message length:\t",data_count)
     
     
-def GetFirmwareIDs(serial) -> None:
+    if ret_hex_int=='none':
+        return
+    elif ret_hex_int=='hex':
+        return received_hex
+    elif ret_hex_int=='int':
+        return received
+    elif ret_hex_int=='both':
+        return received, received_hex
+    
+    
+def GetFirmwareIDs(serial):
     """Get firmware IDs"""
     serial.write(bytearray([0xD2, 0x00, 0xD2]))
-    callback = GeneralSystemMessages(serial)
-    print(callback)
+    SystemMessageCallback(serial)
 
-def SoftwareReset(serial) -> None:
+def SoftwareReset(serial):
     serial.write(bytearray([0xA1, 0x00, 0xA1]))
-    callback = GeneralSystemMessages(serial)
-    print(callback)
+    SystemMessageCallback(serial)
