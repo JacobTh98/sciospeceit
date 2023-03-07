b962c46e83b4615bf45622417af8124b8fd3a6a5
jt292 <jacob.thoenes@uni-rostock.de>
2023-03-07 12:31:27 +0100

    first full good run

--- pyender/run_meas_prototype.py
+++ pyender/run_meas_prototype.py
@@ -10,10 +10,8 @@ import json
 
 from sciopy import (
     SystemMessageCallback,
-    #    configuration_01,
-    #    configuration_02,
     SetBurstCount,
-    configuration_03,
+    conf_n_el_16_adjacent,
     connect_COM_port,
     StartStopMeasurement,
     reshape_full_message_in_bursts,
@@ -77,9 +75,6 @@ except BaseException:
 
 def check_for_content(
     measurement_data: np.ndarray,
-    COM_ScioSpec,
-    scio_spec_measurement_config: ScioSpecMeasurementConfig,
-    ch_n: int = 0,
 ) -> np.ndarray:
     """Checks the data for content. If data is available continues measurement, else abort script.
 
@@ -113,7 +108,7 @@ def check_for_content(
 #     SoftwareReset(COM_ScioSpec)
 #     time.sleep(5)
 #     COM_ScioSpec = connect_COM_port()
-#     scio_spec_measurement_config = configuration_03(
+#     scio_spec_measurement_config = conf_n_el_16_adjacent(
 #         COM_ScioSpec, scio_spec_measurement_config
 #     )
 #     SetBurstCount(COM_ScioSpec, scio_spec_measurement_config)
@@ -165,12 +160,12 @@ if accessed:
         enderstat,
     ) = split_pickle_to_classes(meas_cnf)
     override_burst_count = scio_spec_measurement_config.burst_count
-    print(
-        scio_spec_measurement_config,
-        circledrivepattern,
-        kartesiandrivepattern,
-        enderstat,
-    )
+    # print(
+    #    scio_spec_measurement_config,
+    #    circledrivepattern,
+    #    kartesiandrivepattern,
+    #    enderstat,
+    # )
     # Check if the measurement before was saved:
     if (
         scio_spec_measurement_config.actual_sample != 0
@@ -188,16 +183,16 @@ if accessed:
     except ConnectionError:
         print("Cant connect to port.")
 
-    if files_offset % 100 == 0:
-        telegram_KI_bot("preventive Reset", telegram_config)
-        print("Preventive SoftwareReset")
-        SoftwareReset(COM_ScioSpec)
-        time.sleep(5)
-        COM_ScioSpec = connect_COM_port()
-        SystemMessageCallback(COM_ScioSpec)
+    # if files_offset % 500 == 0:
+    #    telegram_KI_bot("preventive reset", telegram_config)
+    #    print("Preventive SoftwareReset")
+    #    SoftwareReset(COM_ScioSpec)
+    #    time.sleep(5)
+    #    COM_ScioSpec = connect_COM_port()
+    #    SystemMessageCallback(COM_ScioSpec)
 
     # Send configuration an read answer
-    scio_spec_measurement_config = configuration_03(
+    scio_spec_measurement_config = conf_n_el_16_adjacent(
         COM_ScioSpec, scio_spec_measurement_config
     )
     print("\tConfig 4", scio_spec_measurement_config)
@@ -228,7 +223,36 @@ if accessed:
         measurement_data = split_bursts_in_frames(
             split_measurement_data, scio_spec_measurement_config
         )
-        measurement_data = check_for_content(measurement_data)
+        print(f"\tlen of {len(measurement_data[0])=}.")
+        if len(measurement_data[0]) == 0:
+            telegram_KI_bot("empty measurement->reset", telegram_config)
+            SystemMessageCallback(COM_ScioSpec)
+            time.sleep(5)
+            SoftwareReset(COM_ScioSpec)
+            time.sleep(10)
+            COM_ScioSpec = connect_COM_port(COM_ScioSpec.name)
+            time.sleep(2)
+            SystemMessageCallback(COM_ScioSpec)
+            scio_spec_measurement_config = conf_n_el_16_adjacent(
+                COM_ScioSpec, scio_spec_measurement_config
+            )
+            time.sleep(1)
+            SystemMessageCallback(COM_ScioSpec)
+            SetBurstCount(COM_ScioSpec, scio_spec_measurement_config)
+            SystemMessageCallback(COM_ScioSpec)
+            # Measure up to burst count
+            measurement_data_hex = StartStopMeasurement(COM_ScioSpec)
+            # Delete hex in mesured buffer
+            measurement_data = del_hex_in_list(measurement_data_hex)
+            # Reshape the full mesaurement buffer. Depending on number of electrodes
+            split_measurement_data = reshape_full_message_in_bursts(
+                measurement_data, scio_spec_measurement_config
+            )
+            measurement_data = split_bursts_in_frames(
+                split_measurement_data, scio_spec_measurement_config
+            )
+
+        # measurement_data = check_for_content(measurement_data)
 
         for bursts in measurement_data:
             # Check if burst is zero:
