b04c2ef5c0653c090ba9a51aface7ad027aef1a9
Jacob Peter Thoenes <jacob.thoenes@uni-rostock.de>
2023-03-31 09:44:44 +0200

    Revert "Merge branch 'jac_dev' into 'main'"
    This reverts merge request !7

--- pyender/run_meas_prototype.py
+++ pyender/run_meas_prototype.py
@@ -1,17 +1,16 @@
 import time
 import os
 import numpy as np
-
-# import sys
+import sys
 import pickle
 from typing import Union
-import requests
-import json
 
 from sciopy import (
     SystemMessageCallback,
+    #    configuration_01,
+    #    configuration_02,
     SetBurstCount,
-    conf_n_el_16_adjacent,
+    configuration_03,
     connect_COM_port,
     StartStopMeasurement,
     reshape_full_message_in_bursts,
@@ -25,56 +24,11 @@ from ender_sciospec_classes import CircleDrivePattern, KartesianDrivePattern, En
 from sciopy.sciopy_dataclasses import ScioSpecMeasurementConfig
 
 
-def read_telegram_json(json_path: str = "telegram_config.json") -> dict:
-    """
-    Insert the "token", "chat_id" inside the 'telegram_config.json'
-    and set "state":True for using the telegram progress bar.
-
-    Parameters
-    ----------
-    json_path : str, optional
-        path to configuration file, by default 'telegram_config.json'
-
-    Returns
-    -------
-    dict
-        configuration
-    """
-    conf_file = open(json_path)
-    telegram_config = json.load(conf_file)
-    return telegram_config
-
-
-def telegram_KI_bot(bot_message: str, bot_cnf: dict) -> None:
-    """
-    telegram_KI_bot sends a message string to a configured telegram bot.
-
-    Parameters
-    ----------
-    bot_message : str
-        message to send
-    bot_cnf : dict
-        bot configuration
-    """
-    send_text = (
-        "https://api.telegram.org/bot"
-        + bot_cnf["token"]
-        + "/sendMessage?chat_id="
-        + bot_cnf["chat_id"]
-        + "&parse_mode=Markdown&text="
-        + bot_message
-    )
-    requests.get(send_text)
-
-
-try:
-    telegram_config = read_telegram_json(json_path="telegram_config.json")
-except BaseException:
-    print("Error 404: No telegram config found")
-
-
 def check_for_content(
     measurement_data: np.ndarray,
+    COM_ScioSpec,
+    scio_spec_measurement_config: ScioSpecMeasurementConfig,
+    ch_n: int = 0,
 ) -> np.ndarray:
     """Checks the data for content. If data is available continues measurement, else abort script.
 
@@ -94,10 +48,34 @@ def check_for_content(
     np.ndarray
         measurement data
     """
-    # ch_n = ch_n
+    ch_n = ch_n
     if len(measurement_data[0]) == 0:
-        telegram_KI_bot("Message is empty", telegram_config)
-        print("empty buffer")
+        if ch_n == 4:
+            sys.exit("ScioSpec devices has crashed.")
+        else:
+            SoftwareReset(COM_ScioSpec)
+            time.sleep(5)
+            COM_ScioSpec = connect_COM_port()
+            scio_spec_measurement_config = configuration_03(
+                COM_ScioSpec, scio_spec_measurement_config
+            )
+            SetBurstCount(COM_ScioSpec, scio_spec_measurement_config)
+            SystemMessageCallback(COM_ScioSpec)
+            # Measure up to burst count
+            measurement_data_hex = StartStopMeasurement(COM_ScioSpec)
+            # Delete hex in mesured buffer
+            measurement_data = del_hex_in_list(measurement_data_hex)
+            # Reshape the full mesaurement buffer. Depending on number of electrodes
+            split_measurement_data = reshape_full_message_in_bursts(
+                measurement_data, scio_spec_measurement_config
+            )
+            measurement_data = split_bursts_in_frames(
+                split_measurement_data, scio_spec_measurement_config
+            )
+            ch_n += 1
+            check_for_content(
+                measurement_data, COM_ScioSpec, scio_spec_measurement_config, ch_n
+            )
     else:
         return measurement_data
 
@@ -110,6 +88,7 @@ def split_pickle_to_classes(
 ) -> Union[
     ScioSpecMeasurementConfig, CircleDrivePattern, KartesianDrivePattern, Ender5Stat
 ]:
+
     return pkl_data[0], pkl_data[1], pkl_data[2], pkl_data[3]
 
 
@@ -131,12 +110,12 @@ if accessed:
         enderstat,
     ) = split_pickle_to_classes(meas_cnf)
     override_burst_count = scio_spec_measurement_config.burst_count
-    # print(
-    #    scio_spec_measurement_config,
-    #    circledrivepattern,
-    #    kartesiandrivepattern,
-    #    enderstat,
-    # )
+    print(
+        scio_spec_measurement_config,
+        circledrivepattern,
+        kartesiandrivepattern,
+        enderstat,
+    )
     # Check if the measurement before was saved:
     if (
         scio_spec_measurement_config.actual_sample != 0
@@ -154,16 +133,8 @@ if accessed:
     except ConnectionError:
         print("Cant connect to port.")
 
-    # if files_offset % 500 == 0:
-    #    telegram_KI_bot("preventive reset", telegram_config)
-    #    print("Preventive SoftwareReset")
-    #    SoftwareReset(COM_ScioSpec)
-    #    time.sleep(5)
-    #    COM_ScioSpec = connect_COM_port()
-    #    SystemMessageCallback(COM_ScioSpec)
-
     # Send configuration an read answer
-    scio_spec_measurement_config = conf_n_el_16_adjacent(
+    scio_spec_measurement_config = configuration_03(
         COM_ScioSpec, scio_spec_measurement_config
     )
     print("\tConfig 4", scio_spec_measurement_config)
@@ -194,37 +165,7 @@ if accessed:
         measurement_data = split_bursts_in_frames(
             split_measurement_data, scio_spec_measurement_config
         )
-        print(f"\tlen of {len(measurement_data[0])=}.")
-
-        reset_num = 0
-        while len(measurement_data[0]) == 0:
-            telegram_KI_bot(f"empty measurement->reset {reset_num}", telegram_config)
-            SystemMessageCallback(COM_ScioSpec)
-            time.sleep(5)
-            SoftwareReset(COM_ScioSpec)
-            time.sleep(10)
-            COM_ScioSpec = connect_COM_port(COM_ScioSpec.name)
-            time.sleep(2)
-            SystemMessageCallback(COM_ScioSpec)
-            scio_spec_measurement_config = conf_n_el_16_adjacent(
-                COM_ScioSpec, scio_spec_measurement_config
-            )
-            time.sleep(1)
-            SystemMessageCallback(COM_ScioSpec)
-            SetBurstCount(COM_ScioSpec, scio_spec_measurement_config)
-            SystemMessageCallback(COM_ScioSpec)
-            # Measure up to burst count
-            measurement_data_hex = StartStopMeasurement(COM_ScioSpec)
-            # Delete hex in mesured buffer
-            measurement_data = del_hex_in_list(measurement_data_hex)
-            # Reshape the full mesaurement buffer. Depending on number of electrodes
-            split_measurement_data = reshape_full_message_in_bursts(
-                measurement_data, scio_spec_measurement_config
-            )
-            measurement_data = split_bursts_in_frames(
-                split_measurement_data, scio_spec_measurement_config
-            )
-            reset_num += 1
+        measurement_data = check_for_content(measurement_data)
 
         for bursts in measurement_data:
             # Check if burst is zero:
